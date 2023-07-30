5aedce483a9837c89c5da9894abcbc3f9b2a526f
Zach Kelling <z@zeekay.io>
2023-07-30 11:05:07 -0700

    Update logo

--- YApp/About.swift
+++ YApp/About.swift
@@ -2,23 +2,23 @@ import Cocoa
 
 class About {
   private let familyCredits = NSAttributedString(
-    string: "Special thank you to Tonia & Guy! ❤️",
+    string: "Special thank Anthropic & Hugging Face! ❤️",
     attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor]
   )
 
   private var kossCredits: NSMutableAttributedString {
-    let string = NSMutableAttributedString(string: "Kudos to Sasha Koss for help! 🏂",
+    let string = NSMutableAttributedString(string: "Donate to save animals! 🏂",
                                            attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
-    string.addAttribute(.link, value: "https://koss.nocorp.me", range: NSRange(location: 9, length: 10))
+    string.addAttribute(.link, value: "https://zoo.ngo", range: NSRange(location: 9, length: 10))
     return string
   }
 
   private var links: NSMutableAttributedString {
     let string = NSMutableAttributedString(string: "Website│GitHub│Support",
                                            attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
-    string.addAttribute(.link, value: "https://yapp.app", range: NSRange(location: 0, length: 7))
-    string.addAttribute(.link, value: "https://github.com/zeekay/YApp", range: NSRange(location: 8, length: 6))
-    string.addAttribute(.link, value: "mailto:support@yapp.app", range: NSRange(location: 15, length: 7))
+    string.addAttribute(.link, value: "https://yapp.io", range: NSRange(location: 0, length: 7))
+    string.addAttribute(.link, value: "https://github.com/zeekay/yapp", range: NSRange(location: 8, length: 6))
+    string.addAttribute(.link, value: "mailto:support@yapp.io", range: NSRange(location: 15, length: 7))
     return string
   }
 
